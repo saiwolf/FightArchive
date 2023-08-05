fa87279adbe49dfb2b9856f2dd88ed939e45c560
Robert E. Cato <robertcato015@gmail.com>
2023-08-05 15:34:00 -0500

    Adding Contenders

--- Data/Entities/BaseEntity.cs
+++ Data/Entities/BaseEntity.cs
@@ -7,6 +7,6 @@ public abstract class BaseEntity
     [Key]
     public Guid Id { get; set; }
     [Required]
-    public DateTime Created { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
     public DateTime? Updated { get; set; }
 }
--- Data/Entities/Contender.cs
+++ Data/Entities/Contender.cs
@@ -4,9 +4,10 @@ namespace FightArchive.Data.Entities;
 
 public class Contender : BaseEntity
 {
-    [Required]
+    [Required(ErrorMessage = "Please give your fighter a name!")]
     public string? Name { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Please give us a link for more info about your fighter!")]
+    [DataType(DataType.Url)]
     public string? ProfileUrl { get; set; }
 
     public List<Fight>? Fights { get; set; }
